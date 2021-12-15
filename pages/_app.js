2f83a06dc811679d88994584f2bd3d36c47ef4ef
shyam-mahato <shyammahato413@gmail.com>
2021-12-15 14:51:03 +0530

    from shyam

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './../components/header';
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <><Header></Header><Component {...pageProps} /></>
+  )
+}
+
+export default MyApp
